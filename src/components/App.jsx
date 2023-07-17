94c507ebad7ab7123a32044f3ad22ac6776f904e
rafalbarczak <rafal.barczak@intmail.pl>
2023-07-17 17:01:31 +0200

    added saving in localStorage

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -7,13 +7,27 @@ import { ContactFilter } from './ContactFilter/ContactFilter';
 export class App extends Component {
   state = {
     contacts: [
-      { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
-      { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
-      { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
-      { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
+      // { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
+      // { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
+      // { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
+      // { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
     ],
     filter: '',
   };
+
+  componentDidMount() {
+    const storedData = localStorage.getItem('contacts');
+
+    if (storedData) {
+      this.setState({ contacts: JSON.parse(storedData) });
+    }
+  }
+
+  componentDidUpdate(prevState) {
+    if (prevState.contacts !== this.state.contacts) {
+      localStorage.setItem('contacts', JSON.stringify(this.state.contacts));
+    }
+  }
 
   handleChange = e => {
     const { name, value } = e.target;
